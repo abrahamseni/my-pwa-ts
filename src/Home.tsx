5659428df7805c2cf867acb8aa31e96ae5a2d9d3
abrahamseni <aagung@reapit.com>
2022-05-25 15:33:23 +0700

    fix/ui

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,11 +6,21 @@ import {
   CardHeading,
   CardHeadingWrap,
   CardImageWrap,
+  CardListHeading,
+  CardListIcon,
+  CardListItem,
+  CardListItemTextPrimary,
+  CardListItemTextSecondary,
+  CardListItemTextWrap,
+  CardListMainWrap,
   CardMainWrap,
   CardSubHeading,
   CardWrap,
-  elRowGap3,
+  elHasGreyBg,
+  elRowGap6,
+  elW11,
   FlexContainer,
+  Icon,
   Loader,
   PageContainer,
   PersistantNotification,
@@ -22,8 +32,9 @@ import { useGetListings } from './getListings'
 
 export default function Home() {
   const { data: listings, status, isLoading } = useGetListings();
+
   return (
-    <PageContainer>
+    <PageContainer style={{ background: "var(--color-grey-light)" }}>
       <Title>PWA Agency</Title>
       <Subtitle>Property Listings</Subtitle>
       {isLoading ? (
@@ -33,25 +44,96 @@ export default function Home() {
           Error get listings, please try again later.
         </PersistantNotification>
       ) : (
-        <FlexContainer isFlexColumn className={elRowGap3}>
+        <FlexContainer style={{ gap: "16px" }}>
           {listings.map((listing: any) => {
             return (
-              <CardWrap key={listing.id}>
+              <CardWrap key={listing.id} className={elW11}>
                 <CardMainWrap>
                   <CardImageWrap>
                     <img src={listing.image} alt={listing.type} />
                   </CardImageWrap>
+
                   <CardHeadingWrap>
-                    <CardHeading>PWA Agency</CardHeading>
-                    <CardSubHeading>{listing.price}</CardSubHeading>
+                    <CardHeading>{listing.price}</CardHeading>
+                    <CardSubHeading>
+                      {listing.address.streetAddress}
+                    </CardSubHeading>
                   </CardHeadingWrap>
                 </CardMainWrap>
+
                 <CardBodyWrap>
-                  <BodyText>{listing.address.streetAddress}</BodyText>
                   <BodyText>
-                    {listing.address.region} - {listing.address.country}
+                    {listing.address.region}, {listing.address.country}
                   </BodyText>
                 </CardBodyWrap>
+
+                <CardListMainWrap>
+                  <CardListHeading>Property Details</CardListHeading>
+                </CardListMainWrap>
+
+                <CardListItem>
+                  <CardListIcon>
+                    <Icon icon="houseInfographic" />
+                  </CardListIcon>
+                  <CardListItemTextWrap>
+                    <CardListItemTextPrimary>
+                      {listing.label}
+                    </CardListItemTextPrimary>
+                    <CardListItemTextSecondary>
+                      {listing.type}
+                    </CardListItemTextSecondary>
+                  </CardListItemTextWrap>
+                </CardListItem>
+
+                <CardListItem>
+                  <CardListIcon>
+                    <Icon icon="houseInfographic" />
+                  </CardListIcon>
+                  <CardListItemTextWrap>
+                    <CardListItemTextPrimary>Bed:</CardListItemTextPrimary>
+                    <CardListItemTextSecondary>
+                      {listing.bed}
+                    </CardListItemTextSecondary>
+                  </CardListItemTextWrap>
+                </CardListItem>
+
+                <CardListItem>
+                  <CardListIcon>
+                    <Icon icon="houseInfographic" />
+                  </CardListIcon>
+                  <CardListItemTextWrap>
+                    <CardListItemTextPrimary>Bath:</CardListItemTextPrimary>
+                    <CardListItemTextSecondary>
+                      {listing.bath}
+                    </CardListItemTextSecondary>
+                  </CardListItemTextWrap>
+                </CardListItem>
+
+                <CardListItem>
+                  <CardListIcon>
+                    <Icon icon="carSolidSystem" />
+                  </CardListIcon>
+                  <CardListItemTextWrap>
+                    <CardListItemTextPrimary>Car Park:</CardListItemTextPrimary>
+                    <CardListItemTextSecondary>
+                      {listing.car}
+                    </CardListItemTextSecondary>
+                  </CardListItemTextWrap>
+                </CardListItem>
+
+                <CardListItem>
+                  <img
+                    src={listing.primaryAgent?.photo}
+                    alt={listing.primaryAgent.fullName}
+                    style={{ borderRadius: "4px", marginRight: "10px" }}
+                  />
+                  <CardListItemTextWrap>
+                    <CardListItemTextPrimary>
+                      {listing.primaryAgent?.fullName}
+                    </CardListItemTextPrimary>
+                    <CardListItemTextSecondary>Agent</CardListItemTextSecondary>
+                  </CardListItemTextWrap>
+                </CardListItem>
               </CardWrap>
             );
           })}
